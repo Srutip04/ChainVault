6cc98e174c867f8289c7f20276aaee50fae37391
Srutip04 <subuhishruti75@gmail.com>
2024-01-19 18:27:21 +0530

    server code

--- server/server.js
+++ server/server.js
@@ -1,14 +1,57 @@
 // server.js
 const express = require('express');
-const Web3 = require('web3');
+const { Web3 }  = require('web3');
 const app = express();
 const port = 3001;
 
-const web3 = new Web3('YOUR_ETH_NODE_URL');
-const contractAddress = 'YOUR_CONTRACT_ADDRESS';
-const abi = [...]; // ABI of your deployed smart contract
+const web3 = new Web3("http://127.0.0.1:7545");
+// console.log(web3);
+// const web3 = new Web3(new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545"));
+const contractAddress = "0x2Ff3D0b47Ef3E851a2059644ecB80B4C76fF2326";
+const abi = [
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_data",
+        type: "string",
+      },
+    ],
+    name: "set",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "get",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "storedData",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+]; // ABI of your deployed smart contract
 
 const contract = new web3.eth.Contract(abi, contractAddress);
+// console.log(contract);
 
 app.use(express.json());
 
@@ -19,7 +62,9 @@ app.get('/api/get', async (req, res) => {
 
 app.post('/api/set', async (req, res) => {
     const { data } = req.body;
-    await contract.methods.set(data).send({ from: 'YOUR_WALLET_ADDRESS' });
+    await contract.methods
+      .set(data)
+      .send({ from: "0xc6C406aF379f7c80cE5A4f314462Ef45577Bc286" });
     res.json({ message: 'Data set successfully' });
 });
 
